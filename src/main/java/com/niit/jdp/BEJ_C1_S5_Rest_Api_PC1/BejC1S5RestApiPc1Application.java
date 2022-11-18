69edf9ace444938ae64375a51978f6fcd408eb4f
Praveen Kumar <praveen170596@gmail.com>
2022-11-18 19:30:56 +0530

    initial commit

---
+++ src/main/java/com/niit/jdp/BEJ_C1_S5_Rest_Api_PC1/BejC1S5RestApiPc1Application.java
@@ -0,0 +1,13 @@
+package com.niit.jdp.BEJ_C1_S5_Rest_Api_PC1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BejC1S5RestApiPc1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BejC1S5RestApiPc1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/niit/jdp/BEJ_C1_S5_Rest_Api_PC1/BejC1S5RestApiPc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit.jdp.BEJ_C1_S5_Rest_Api_PC1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BejC1S5RestApiPc1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
