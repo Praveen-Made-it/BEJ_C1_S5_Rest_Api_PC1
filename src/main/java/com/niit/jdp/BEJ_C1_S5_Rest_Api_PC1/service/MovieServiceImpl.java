d91323f1742b1c25464f7a6e4ccc1b2ee4811b3c
Praveen Kumar <praveen170596@gmail.com>
2022-11-18 19:58:02 +0530

    implemented methods from MovieService

--- src/main/java/com/niit/jdp/BEJ_C1_S5_Rest_Api_PC1/service/MovieServiceImpl.java
+++ src/main/java/com/niit/jdp/BEJ_C1_S5_Rest_Api_PC1/service/MovieServiceImpl.java
@@ -7,5 +7,24 @@
 
 package com.niit.jdp.BEJ_C1_S5_Rest_Api_PC1.service;
 
-public class MovieServiceImpl {
+import com.niit.jdp.BEJ_C1_S5_Rest_Api_PC1.domain.Movie;
+
+import java.util.List;
+
+public class MovieServiceImpl implements MovieService {
+
+    @Override
+    public Movie saveMovie(Movie movie) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteMovie(int movieId) {
+        return false;
+    }
+
+    @Override
+    public List<Movie> fetchAllMovies() {
+        return null;
+    }
 }
