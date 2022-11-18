b9e8dc112600c2639317e994b7dd5bc740da59d3
Praveen Kumar <praveen170596@gmail.com>
2022-11-18 20:14:32 +0530

    created constructor for MovieService

--- src/main/java/com/niit/jdp/BEJ_C1_S5_Rest_Api_PC1/controller/MovieController.java
+++ src/main/java/com/niit/jdp/BEJ_C1_S5_Rest_Api_PC1/controller/MovieController.java
@@ -7,5 +7,18 @@
 
 package com.niit.jdp.BEJ_C1_S5_Rest_Api_PC1.controller;
 
+import com.niit.jdp.BEJ_C1_S5_Rest_Api_PC1.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/")
 public class MovieController {
+    private MovieService movieService;
+
+    @Autowired
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
 }
