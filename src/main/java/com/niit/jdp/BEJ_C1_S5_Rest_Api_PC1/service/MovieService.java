9bcb318713374d28417a46270cc02f46f59c7bb2
Praveen Kumar <praveen170596@gmail.com>
2022-11-18 19:56:03 +0530

    created methods to saveMove , deleteMovie ,fetchAllMovies from the Database

--- src/main/java/com/niit/jdp/BEJ_C1_S5_Rest_Api_PC1/service/MovieService.java
+++ src/main/java/com/niit/jdp/BEJ_C1_S5_Rest_Api_PC1/service/MovieService.java
@@ -1,4 +1,14 @@
 package com.niit.jdp.BEJ_C1_S5_Rest_Api_PC1.service;
 
+import com.niit.jdp.BEJ_C1_S5_Rest_Api_PC1.domain.Movie;
+
+import java.util.List;
+
 public interface MovieService {
+
+    Movie saveMovie(Movie movie);
+
+    boolean deleteMovie(int movieId);
+
+    List<Movie> fetchAllMovies();
 }
