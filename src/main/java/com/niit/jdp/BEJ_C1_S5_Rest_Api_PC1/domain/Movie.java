94886b9eff4132cdcbcc94a22d2ea316ddc58c78
Praveen Kumar <praveen170596@gmail.com>
2022-11-18 19:38:45 +0530

    created Movie class

---
+++ src/main/java/com/niit/jdp/BEJ_C1_S5_Rest_Api_PC1/domain/Movie.java
@@ -0,0 +1,11 @@
+/*
+ *Author Name:Praveen Kumar
+ *Date: 18-Nov-22
+ *Created With IntelliJ Idea Community Edition
+ */
+
+
+package com.niit.jdp.BEJ_C1_S5_Rest_Api_PC1.domain;
+
+public class Movie {
+}
