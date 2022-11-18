143b9ac25b41e2bcddc01fd8b77f7a3cc9e149aa
Praveen Kumar <praveen170596@gmail.com>
2022-11-18 19:43:15 +0530

    created constructor and annotations added

--- src/main/java/com/niit/jdp/BEJ_C1_S5_Rest_Api_PC1/domain/Movie.java
+++ src/main/java/com/niit/jdp/BEJ_C1_S5_Rest_Api_PC1/domain/Movie.java
@@ -7,9 +7,26 @@
 
 package com.niit.jdp.BEJ_C1_S5_Rest_Api_PC1.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Movie {
+    @Id
     private int movieId;
     private String movieName;
     private String actorName;
     private String movieDirector;
+
+    public Movie() {
+    }
+
+    public Movie(int movieId, String movieName, String actorName, String movieDirector) {
+        this.movieId = movieId;
+        this.movieName = movieName;
+        this.actorName = actorName;
+        this.movieDirector = movieDirector;
+    }
+
+
 }
